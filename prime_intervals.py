7c99aa1be0badad7c848ceacafcd7afd87af8af0
ASravanthi1 <41766108+ASravanthi1@users.noreply.github.com>
2018-08-14 16:10:24 +0530

    Create prime_intervals.py

---
+++ prime_intervals.py
@@ -0,0 +1,13 @@
+lower,upper=map(int,input().split())
+for i in range(lower,upper+1):
+    count=0;l=0;c=0
+    for j in range(1,i+1):
+        if(i%j==0):
+            count+=1
+    if(count==2):
+        if(c==0):
+            print(i,end=" ")
+        else:
+            print("",end=" ")
+            print(i,end="")
+        l+=1
