9d6bfc8461f029c960c78f84a69922e59ece50c9
ASravanthi1 <41766108+ASravanthi1@users.noreply.github.com>
2018-08-27 11:06:03 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -2,14 +2,21 @@ n=int(input(""))
 a=1
 b=1
 count=0
+c=0
+k=" "
 if(n==0):
     print(a)
 elif(n<0):
     print("negative number")
 else:
     while(count<n):
-        print(a,sep=' ',end=' ')
+        if(c==0):
+            print(a,end="")
+        else:
+            print("",end=k)
+            print(a,end="")
         nexterm=a+b
         a=b
         b=nexterm
         count+=1
+        c+=1
